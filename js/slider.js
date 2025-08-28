0191dcdc3f968aa2567b4e69f69cf3d79d50aa8f
ringo-117 <168697072+ringo-117@users.noreply.github.com>
2025-08-28 09:48:31 +0900

    update-18　ヒーロースライド追加

--- js/slider.js
+++ js/slider.js
@@ -37,3 +37,14 @@ const swiper = new Swiper(".swiper", {
     }
   }
 });
+
+// ヒーロースライダー
+const heroSwiper = new Swiper(".hero-swiper", {
+  effect: "fade",       // フェード切り替え
+  loop: true,           // ループ
+  autoplay: {
+    delay: 3400,        // 3秒ごとに切り替え
+    disableOnInteraction: false,
+  },
+  speed: 2500,          // フェードの速さ（1秒）
+});
