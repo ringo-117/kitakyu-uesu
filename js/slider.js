1fce46ead30a2fa6d8cad19a312c37e9dc15ee9b
ringo-117 <168697072+ringo-117@users.noreply.github.com>
2025-09-02 13:47:36 +0900

    update-20　スライダー修正

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,20 @@
 const slideCount = document.querySelectorAll('.swiper-slide').length;
-// const enableLoop = slideCount > 5;
+// const enableLoop = slideCount > 4;
 const swiper = new Swiper(".swiper", {
+  // slidesPerView: "auto",
   slidesPerView: 1.2,
-  // centeredSlides: true,
+  slidesPerGroup: 1,
+  centeredSlides: true,
   spaceBetween: 10,
   grabCursor: true,
-  // loop: true,
+  loop: false,
   effect: 'slide',
   speed: 800,
+  // watchOverflow: false,
 
   // pagination: {
   //   el: ".swiper-pagination",
+  //    type: "fraction",
   // },
   navigation: {
     nextEl: ".swiper-button-next",
@@ -22,15 +26,20 @@ const swiper = new Swiper(".swiper", {
     // 0px以上（スマホなど）
     0: {
       slidesPerView: 1.2,
+      // slidesPerView: "auto",
+      // slidesPerGroup: 1,
       spaceBetween: 20,
     },
     // 640px以上（タブレットなど）
     640: {
       slidesPerView: 1.5,
+      // slidesPerView: "auto",
+      // slidesPerGroup: 1,
       spaceBetween: 25,
       // centeredSlides: false,
 
-    },
+    }
+    ,
     // 1024px以上（PC）
     1024: {
       enabled: false,
@@ -38,7 +47,8 @@ const swiper = new Swiper(".swiper", {
   }
 });
 
-// ヒーロースライダー
+
+// // ヒーロースライダー
 const heroSwiper = new Swiper(".hero-swiper", {
   effect: "fade",       // フェード切り替え
   loop: true,           // ループ
